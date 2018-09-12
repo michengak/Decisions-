f5ad7aa57025daf875c822535568cdd975c2b64e
michel ngako <emoumichel@gmail.com>
2018-09-12 00:33:29 -0400

    Add project files.

---
+++ Decisions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.WriteLine("Choose a door: 1, 2, or 3: ");
+            string userVAlue = Console.ReadLine();
+
+            if (userVAlue == "1")
+            {
+                string message = "You won a new car!";
+                Console.WriteLine(message);
+
+            }
+            Console.ReadLine();
+
+        }   
+
+
+
+    }
+}
